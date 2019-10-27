9d1ad6b3e0e8c3f552319fb51437852bb09253aa
maddogmitch <mightymitch66@gmail.com>
2019-10-26 21:03:45 -0400

    First Commit

---
+++ Assets/Scripts/Enemies Code/Left To Right.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftToRight : MonoBehaviour
+{
+    public Transform start;
+    public Transform end;
+    public Transform enemy;
+    public int speed;
+    bool endP = false;
+    bool startP = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // which empty objectg it will move to is set
+        if (enemy.position.x >= end.position.x)
+        {
+            endP = true;
+            startP = false;
+        }
+
+        if (enemy.position.x <= start.position.x)
+        {
+            endP = false;
+            startP = true;
+        }
+
+        //which way the enemy moves
+        if (enemy.position.x <= end.position.x && endP == false)
+        {
+            transform.position += Vector3.right * Time.deltaTime * speed;
+        }
+        if (enemy.position.x >= start.position.x && startP == false)
+        {
+            transform.position += Vector3.left * Time.deltaTime * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/Player Code/Player Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D RB;
+
+    public float MoveSpeed = 6f;
+    public float JumpHeight = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            RB.velocity = new Vector2(MoveSpeed, RB.velocity.y);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            RB.velocity = new Vector2(-MoveSpeed, RB.velocity.y);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            RB.velocity = new Vector2(RB.velocity.x, JumpHeight);
+        }
+    }
+}
+
+/*----------------------------------NOTE----------------------------------
+ * add rigidbody to your player so you can use the gravityscale modifier
+ * play around until you think it feeels right
+ --------------------------------------------------------
