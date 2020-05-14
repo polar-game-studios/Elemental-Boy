65af62a39f62370a0ecf4c95b39fdbd313b4dd40
maddogmitch <mightymitch66@gmail.com>
2020-05-14 13:39:44 -0400

    Added Game Manager

---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject levelStart;
+    public GameObject levelEnd;
+    public GameObject elementalBoy;
+
+    public GameObject SettingsPanel;
+    public GameObject GameOverPanel;
+
+    public int playerLives;
+    public float playerHealth;
+
+    //Moves the player to the next level
+    public void nextLevel()
+    {
+
+    }
+
+    //What must occur if the player dies
+    public void playerDeath()
+    {
+
+    }
+
+    //When player lives equals zero
+    public void gameOver()
+    {
+
+    }
+}
